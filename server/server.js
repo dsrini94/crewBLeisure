496684c848968075329ab39a9ab9fba44867eafd
dsrini94 <sendtosrini@outlook.com>
2018-09-27 18:12:19 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ const express = require('express')
     , app = express()
     , server = require('http').Server(app);
 
+const port = process.env.PORT || 8081;
+
 app.use(express.static('./../'));
 
-server.listen(8001, function() {
+server.listen(port, function() {
     console.log('server started on  8001');
 });
