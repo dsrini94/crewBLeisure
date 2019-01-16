4be894105b6c3ffd9ef49e5c19d6e1b47027c983
dsrini94 <sendtosrini@outlook.com>
2019-01-16 17:08:04 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
     , app = express()
     , server = require('http').Server(app);
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8004;
 
 app.use(express.static('./../'));
 
